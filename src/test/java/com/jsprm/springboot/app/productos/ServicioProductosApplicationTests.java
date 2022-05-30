e49ba4941d93d05a405498983d6ee81b92d1fa4a
Jose Alberto Sanchez Pacheco <jasp@ibm.com>
2022-05-29 21:23:54 -0500

    Initial commit

---
+++ src/test/java/com/jsprm/springboot/app/productos/ServicioProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsprm.springboot.app.productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioProductosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
