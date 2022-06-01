7f43b5ac71cbef74124b7bf3ed8d65f438983f1e
Jose Alberto Sanchez Pacheco <jasp@ibm.com>
2022-05-31 23:57:47 -0500

    DAO design pattern

---
+++ src/test/java/com/jsprm/springboot/app/productos/models/entities/Producto.java
@@ -0,0 +1,24 @@
+package com.jsprm.springboot.app.productos.models.entities;
+
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "productos")
+public class Producto implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private Double precio;
+
+    @Column(name = "fecha_alta")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date fechaAlta;
+
+    @Serial
+    private static final long serialVersionUID = -2187465581222710207L;
+}
---
+++ src/test/java/com/jsprm/springboot/app/productos/services/ProductoServiceImpl.java
@@ -0,0 +1,29 @@
+package com.jsprm.springboot.app.productos.services;
+
+import com.jsprm.springboot.app.productos.models.entities.Producto;
+import com.jsprm.springboot.app.productos.repositories.ProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class ProductoServiceImpl implements ProductoService{
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Producto> findAll() {
+        return productoRepository.findAll();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Producto findById(Long id) {
+        return productoRepository.findById(id).orElse(null);
+    }
+}
